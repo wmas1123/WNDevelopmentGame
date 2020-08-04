117b61dfd8936b11ec2c95dcc52212aa4eb09dd5
wmas1123 <61249615+wmas1123@users.noreply.github.com>
2020-08-04 16:13:46 -0700

    Start of the character class/Initial upload of code

---
+++ Game/Character.java
@@ -0,0 +1,13 @@
+public class Character {
+    private int xCoord;
+    private int yCoord;
+    private double movementSpeed;
+    private double spriteScaling;
+
+    private final int X_COORD_START =
+    private final int Y_COORD_START =
+
+    public Character() {
+        xCoord
+    }
+}
